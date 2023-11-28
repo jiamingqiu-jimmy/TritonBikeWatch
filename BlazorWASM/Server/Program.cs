6a1f526a0a01157083a0d358b6c1f9323e6acd35
Jimmy <10386521+jiamingqiu-jimmy@users.noreply.github.com>
2023-11-27 18:00:26 -0800

    Published with Azure Vault Keys!

--- BlazorWASM/Server/Program.cs
+++ BlazorWASM/Server/Program.cs
@@ -1,12 +1,26 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using BlazorWASM.Server.DB;
 using BlazorWASM.Server.Initialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
-using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string cosmosPrimaryKey;
+
+var keyVaultName = "TBW-KV";
+var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+var secretClient = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+// Assuming "MySecret" is the name of the secret in Azure Key Vault
+KeyVaultSecret secret = await secretClient.GetSecretAsync("CosmosDBPrimaryKey");
+
+// Access the secret value
+cosmosPrimaryKey = secret.Value;
 
 // Add services to the container.
 
@@ -16,9 +30,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddDependencies();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseCosmos(builder.Configuration.GetSection("CosmosDB:EndpointUri").Value, builder.Configuration.GetSection("CosmosDbSettings:PrimaryKey").Value, databaseName: "Core"));
+    options.UseCosmos(builder.Configuration.GetSection("CosmosDB:EndpointUri").Value, cosmosPrimaryKey, databaseName: "Core"));
 
-var cosmosClient = new CosmosClient(builder.Configuration.GetSection("CosmosDB:EndpointUri").Value, builder.Configuration.GetSection("CosmosDbSettings:PrimaryKey").Value);
+var cosmosClient = new CosmosClient(builder.Configuration.GetSection("CosmosDB:EndpointUri").Value, cosmosPrimaryKey);
 Database database = cosmosClient.CreateDatabaseIfNotExistsAsync("Core").Result;
 
 database.CreateContainerIfNotExistsAsync("Alerts", "/Id").Wait();
@@ -28,6 +42,7 @@ database.CreateContainerIfNotExistsAsync("Users", "/Id").Wait();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+
 
 var app = builder.Build();
 
@@ -58,6 +73,3 @@ app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.Run();
-
-IdentityModelEventSource.ShowPII = true;
-IdentityModelEventSource.LogCompleteSecurityArtifact = true;
