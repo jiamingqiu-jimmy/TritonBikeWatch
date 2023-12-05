5cf5816f2f827ce933efab954dfd395ef5d94346
Jimmy <10386521+jiamingqiu-jimmy@users.noreply.github.com>
2023-12-04 20:13:12 -0800

    Add popup for main page, and remove duplicate popups being usable

--- BlazorWASM/Server/Controllers/DeviceAlertController.cs
+++ BlazorWASM/Server/Controllers/DeviceAlertController.cs
@@ -18,8 +18,19 @@ namespace BlazorWASM.Server.Controllers
         [HttpGet]
         public async Task<IActionResult> TriggerHubMethod(string deviceName)
         {
+            // Specify the time zone (Pacific Standard Time)
+            TimeZoneInfo pstTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+            // Get the current UTC time
+            DateTime utcNow = DateTime.UtcNow;
+
+            // Convert UTC time to PST time
+            DateTimeOffset pstNow = TimeZoneInfo.ConvertTimeFromUtc(utcNow, pstTimeZone);
+
+            string TimedeviceName = $"{pstNow.ToString("M/d/yyyy h:mm:ss tt")} - {deviceName}";
+
             // Trigger the hub method for all clients
-            await _hubContext.Clients.All.SendAsync("ReceiveMessage", deviceName, $"{deviceName} has sent an Alert!");
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", TimedeviceName, $"{deviceName} has sent an Alert!", true);
 
             return Ok();
         }
--- BlazorWASM/Server/Hubs/ChatHub.cs
+++ BlazorWASM/Server/Hubs/ChatHub.cs
@@ -4,9 +4,20 @@ namespace BlazorWASM.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, bool alert=false)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            // Specify the time zone (Pacific Standard Time)
+            TimeZoneInfo pstTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+            // Get the current UTC time
+            DateTime utcNow = DateTime.UtcNow;
+
+            // Convert UTC time to PST time
+            DateTimeOffset pstNow = TimeZoneInfo.ConvertTimeFromUtc(utcNow, pstTimeZone);
+
+            user = $"{pstNow.ToString("M/d/yyyy h:mm:ss tt")} - {user}";
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message, alert);
         }
     }
 }
